ae73fca59b5d64b5a816a2558e954abcdad5fba7
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-03-17 15:03:17 +0100

    Added sliding sidebar in and out [#171458557]

--- src/components/Layout/components/Sidebar/SideBar.tsx
+++ src/components/Layout/components/Sidebar/SideBar.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import styles from './SideBar.module.scss';
 import { useStateValue } from '../../../../state';
+import { CSSTransition } from 'react-transition-group';
 
 const SideBar: React.SFC = () => {
   const [{ sidebar }, dispatch] = useStateValue();
 
-  return sidebar.isOpen ? (
-    <div className={styles.container}>
-      Sidebar{' '}
-      <button onClick={() => dispatch({ type: 'TOGGLE_SIDEBAR' })}>
-        {sidebar.isOpen ? 'Close' : 'Open'}
-      </button>
-    </div>
-  ) : null;
+  return (
+    <CSSTransition
+      in={sidebar.isOpen}
+      timeout={200}
+      classNames={{
+        enter: styles['slide--enter'],
+        enterDone: styles['slide--enter'],
+        exit: styles['slide--exit'],
+        exitDone: styles['slide--exit'],
+      }}
+    >
+      <div className={`${styles.container} ${styles.slide}`}>
+        Sidebar{' '}
+        <button onClick={() => dispatch({ type: 'TOGGLE_SIDEBAR' })}>
+          {sidebar.isOpen ? 'Close' : 'Open'}
+        </button>
+      </div>
+    </CSSTransition>
+  );
 };
 
 export default SideBar;
