c39f97c7f4059808bd900abff51315c3e3bb35c2
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-03-17 14:46:20 +0100

    Added showing and hiding sidebar [#171458557]

--- src/components/Layout/components/Sidebar/SideBar.tsx
+++ src/components/Layout/components/Sidebar/SideBar.tsx
@@ -5,14 +5,14 @@ import { useStateValue } from '../../../../state';
 const SideBar: React.SFC = () => {
   const [{ sidebar }, dispatch] = useStateValue();
 
-  return (
+  return sidebar.isOpen ? (
     <div className={styles.container}>
       Sidebar{' '}
       <button onClick={() => dispatch({ type: 'TOGGLE_SIDEBAR' })}>
         {sidebar.isOpen ? 'Close' : 'Open'}
       </button>
     </div>
-  );
+  ) : null;
 };
 
 export default SideBar;
