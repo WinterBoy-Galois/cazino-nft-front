a30c7c5480a2c08e30909a2736c2894cfc3fca17
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-03-17 17:09:53 +0100

    Added sidebar opening animation for desktop breakpoint [#171458557]

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,19 +4,30 @@ import TopBar from './components/TopBar';
 import BottomBar from './components/BottomBar';
 import Sidebar from './components/Sidebar';
 import Footer from '../Footer';
+import { useStateValue } from '../../state';
 
 const Layout: React.SFC = ({ children }) => {
+  const [{ sidebar }] = useStateValue();
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.main}>
-        <div className={styles['main__top-bar']}>
+      <div className={`${styles.main} ${sidebar.isOpen ? styles['main--sidebar-open'] : ''}`}>
+        <div
+          className={`${styles['main__top-bar']} ${
+            sidebar.isOpen ? styles['main--sidebar-open'] : ''
+          }`}
+        >
           <TopBar />
         </div>
         <div className={styles.main__content}>
           {children}
           <Footer />
         </div>
-        <div className={styles['main__bottom-bar']}>
+        <div
+          className={`${styles['main__bottom-bar']} ${
+            sidebar.isOpen ? styles['main--sidebar-open'] : ''
+          }`}
+        >
           <BottomBar />
         </div>
       </div>
--- src/components/Layout/components/TopBar/TopBar.tsx
+++ src/components/Layout/components/TopBar/TopBar.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './TopBar.module.scss';
 import Logo from '../../../icons/Logo';
 import SidebarToggle from '../SidebarToggle';
+import { useStateValue } from '../../../../state';
 
 const TopBar: React.SFC = () => {
+  const [{ sidebar }] = useStateValue();
+
   return (
     <div className={`container-fluid h-100`}>
       <div className={`row h-100`}>
@@ -13,7 +16,7 @@ const TopBar: React.SFC = () => {
           </a>
         </div>
         <div className={`col-6 ${styles.center} ${styles.right}`}>
-          <SidebarToggle arrowLeft={true} />
+          {!sidebar.isOpen && <SidebarToggle arrowLeft={true} />}
         </div>
       </div>
     </div>
