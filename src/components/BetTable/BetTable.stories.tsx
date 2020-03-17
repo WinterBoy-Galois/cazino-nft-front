f92fd36e62183989280e1fb75c2e640a046845d5
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-03-17 19:34:03 +0100

    Added ApolloProvider to BetTable story

--- src/components/BetTable/BetTable.stories.tsx
+++ src/components/BetTable/BetTable.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BetTable from '.';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from '../../graphql/client';
 
-storiesOf('Components/BetTable', module).add('default', () => <BetTable />);
+storiesOf('Components/BetTable', module).add('default', () => (
+  <ApolloProvider client={client}>
+    <BetTable />
+  </ApolloProvider>
+));
