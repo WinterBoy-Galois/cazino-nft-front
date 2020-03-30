cfb4eaf26f6d9c21b890e846105d83fa50f75e4f
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-03-30 18:00:31 +0200

    Fixed mocking apollo client in unit test [#171834987]

--- src/components/Layout/components/Sidebar/components/LeaderboardsTab/LeaderboardsTab.test.tsx
+++ src/components/Layout/components/Sidebar/components/LeaderboardsTab/LeaderboardsTab.test.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait, act } from '@testing-library/react';
 import LeaderboardsTab from './LeaderboardsTab';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { createMockClient } from 'mock-apollo-client';
+import { LEADERBOARDS_SUBSCRIPTION } from '../../../../../../graphql/subscriptions';
 
 describe('LeaderboardsTab', () => {
-  it('should match snapshot', () => {
+  it('should match snapshot', async () => {
     // Arrange
+    const mockClient = createMockClient();
+    const queryHandler = jest
+      .fn()
+      .mockResolvedValue({ data: { leaderboardChanged: { daily: [], weekly: [], monthly: [] } } });
+    mockClient.setRequestHandler(LEADERBOARDS_SUBSCRIPTION, queryHandler);
 
     // Act
-    const container = render(<LeaderboardsTab />);
+    const container = render(
+      <ApolloProvider client={mockClient}>
+        <LeaderboardsTab />
+      </ApolloProvider>
+    );
+
+    await act(wait);
 
     // Assert
     expect(container).toMatchSnapshot();
--- src/components/LeaderboardTable/LeaderboardTable.test.tsx
+++ src/components/LeaderboardTable/LeaderboardTable.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import LeaderboardTable from '.';
 
-test.skip('renders', async () => {
+test('renders', async () => {
   const { container } = render(
     <LeaderboardTable leaderboard={[]} isLoading={false} error={false} />
   );
