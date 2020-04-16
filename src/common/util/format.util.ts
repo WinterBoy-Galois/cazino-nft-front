eb2582040fa2ef87450ccd0a8ee2ab4f0485a116
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-04-16 11:48:33 +0200

    Added config to set options for the app [#172161511]

--- src/common/util/format.util.ts
+++ src/common/util/format.util.ts
@@ -1,4 +1,7 @@
 // TIME: time, BET: bet/1000, MULTIPLIER: payout/4, PROFIT: profit/1000
+import { appConfig } from '../config';
+
+const fractionDigits = appConfig.bitcoinFractionDigits;
 
 /**
  * Assume if the value should be divided by 1.000
@@ -12,12 +15,12 @@ const formatBitcoinSmart = (value: string) => {
 };
 
 const formatBitcoin = (value: number) => {
-  return `${value.toFixed(8)}`;
+  return `${value.toFixed(fractionDigits)}`;
 };
 
 const formatBet = (value: number) => {
   const result = value / 1000;
-  return `${result.toFixed(8)}`;
+  return `${result.toFixed(fractionDigits)}`;
 };
 
 const formatMultiplier = (value: number) => {
@@ -27,7 +30,7 @@ const formatMultiplier = (value: number) => {
 
 const formatProfit = (value: number) => {
   const result = value / 1000;
-  return result > 0 ? `+${result.toFixed(8)}` : `${result.toFixed(8)}`;
+  return result > 0 ? `+${result.toFixed(fractionDigits)}` : `${result.toFixed(fractionDigits)}`;
 };
 
 export { formatBet, formatMultiplier, formatProfit, formatBitcoin, formatBitcoinSmart };
