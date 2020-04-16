370a5abc54a80785c3a91dd6fd8d777d4e55483c
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-04-16 06:41:40 +0200

    Removed build warnings

--- src/components/BetTable/BetTable.stories.tsx
+++ src/components/BetTable/BetTable.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BetTable from '.';
-import { ApolloProvider } from '@apollo/react-hooks';
-import client from '../../graphql/client';
 import Bet, { GameTypes } from '../../models/bet';
 
 const bets: Bet[] = [
