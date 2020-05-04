991be696fb8125927ac6a632d19fd72732bcea60
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-05-04 14:31:10 +0200

    Added tests for adding to BetBuffer

--- src/components/BetTable/lib/useBetBuffer.hook.test.ts
+++ src/components/BetTable/lib/useBetBuffer.hook.test.ts
@@ -6,7 +6,7 @@ import Bet from '../../../models/bet';
 describe('useBetBuffer hook', () => {
   it('should instantiate without errors', () => {
     // Arrange
-    const { result } = renderHook(() => useBetBuffer());
+    const { result } = renderHook(() => useBetBuffer({}));
 
     // Act
 
@@ -16,7 +16,7 @@ describe('useBetBuffer hook', () => {
 
   it('should accept new bets via function', () => {
     // Arrange
-    const { result } = renderHook(() => useBetBuffer());
+    const { result } = renderHook(() => useBetBuffer({}));
     const newBets: Bet[] = [
       {
         id: '1518',
@@ -36,5 +36,124 @@ describe('useBetBuffer hook', () => {
 
     // Assert
     expect(result.current.bets.length).toEqual(1);
+  });
+
+  it('should limit bets added in bulk to buffer size', () => {
+    // Arrange
+    const { result } = renderHook(() => useBetBuffer({ bufferSize: 2 }));
+    const newBets: Bet[] = [
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+    ];
+
+    // Act
+    act(() => {
+      result.current.addBets(newBets);
+    });
+
+    // Assert
+    expect(result.current.bets.length).toEqual(2);
+  });
+
+  it('should limit bets added sequentially to buffer size', () => {
+    // Arrange
+    const { result } = renderHook(() => useBetBuffer({ bufferSize: 3 }));
+
+    const newBets: Bet[] = [
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+    ];
+
+    // Act
+    for (let _i: number = 0; _i < 5; _i++) {
+      act(() => {
+        result.current.addBets(newBets);
+      });
+    }
+
+    // Assert
+    expect(result.current.bets.length).toEqual(3);
+  });
+
+  it('should trigger onBetAddedForCurrentUser', () => {
+    // Arrange
+    const mockCallback = jest.fn();
+
+    const { result } = renderHook(() =>
+      useBetBuffer({
+        bufferSize: 3,
+        onBetAddedForCurrentUser: mockCallback,
+      })
+    );
+
+    const newBets: Bet[] = [
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+      {
+        id: '1519',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+      {
+        id: '1520',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 122,
+        username: 'martinezmark',
+      },
+    ];
+
+    // Act
+    act(() => {
+      result.current.addBets(newBets);
+    });
+
+    // Assert
+    expect(mockCallback.mock.calls.length).toBe(2);
   });
 });
--- src/components/BetTable/lib/useBetBuffer.hook.ts
+++ src/components/BetTable/lib/useBetBuffer.hook.ts
@@ -1,11 +1,29 @@
 import Bet from '../../../models/bet';
 import { useState } from 'react';
 
-export const useBetBuffer = () => {
+interface IProps {
+  bufferSize?: number;
+  onBetAddedForCurrentUser?: (bet: Bet) => void;
+}
+
+export const useBetBuffer = ({ bufferSize = 100, onBetAddedForCurrentUser }: IProps) => {
   const [bets, setBets] = useState<Bet[]>([]);
 
   const addBets = (addedBets: Bet[]) => {
-    const newBets = bets ? [...bets, ...addedBets] : [...addedBets];
+    if (onBetAddedForCurrentUser) {
+      addedBets.forEach(bet => {
+        if (bet.userid === 121) {
+          onBetAddedForCurrentUser(bet);
+        }
+      });
+    }
+
+    const newBets = bets
+      ? [
+          ...addedBets.slice(0, bufferSize - addedBets.length),
+          ...bets.slice(0, bufferSize - addedBets.length),
+        ]
+      : [...addedBets.slice(0, bufferSize - addedBets.length)];
     setBets(newBets);
     return newBets;
   };
