ed7b48bc1a45b94c3b3c825867083b92d91be484
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-05-04 13:05:39 +0200

    Added @testing-library/react-hooks to test hooks

--- src/components/BetTable/lib/useBetBuffer.hook.test.ts
+++ src/components/BetTable/lib/useBetBuffer.hook.test.ts
@@ -1,13 +1,40 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
 import { useBetBuffer } from './useBetBuffer.hook';
+import Bet from '../../../models/bet';
 
 describe('useBetBuffer hook', () => {
   it('should instantiate without errors', () => {
     // Arrange
+    const { result } = renderHook(() => useBetBuffer());
 
     // Act
-    const { addBets } = useBetBuffer();
 
     // Assert
-    expect(addBets).toBeInstanceOf(Function);
+    expect(result.current.addBets).toBeInstanceOf(Function);
+  });
+
+  it('should accept new bets via function', () => {
+    // Arrange
+    const { result } = renderHook(() => useBetBuffer());
+    const newBets: Bet[] = [
+      {
+        id: '1518',
+        bet: 0.01855738,
+        gameid: 1,
+        profit: -0.01891901,
+        time: 1588086626434,
+        userid: 121,
+        username: 'martinezmark',
+      },
+    ];
+
+    // Act
+    act(() => {
+      result.current.addBets(newBets);
+    });
+
+    // Assert
+    expect(result.current.bets.length).toEqual(1);
   });
 });
--- src/components/BetTable/lib/useBetBuffer.hook.ts
+++ src/components/BetTable/lib/useBetBuffer.hook.ts
@@ -1,9 +1,14 @@
 import Bet from '../../../models/bet';
+import { useState } from 'react';
 
-export function useBetBuffer() {
-  const addBets = (bets: Bet[]) => {
-    return bets.length;
+export const useBetBuffer = () => {
+  const [bets, setBets] = useState<Bet[]>([]);
+
+  const addBets = (addedBets: Bet[]) => {
+    const newBets = bets ? [...bets, ...addedBets] : [...addedBets];
+    setBets(newBets);
+    return newBets;
   };
 
-  return { addBets };
-}
+  return { bets, addBets };
+};
