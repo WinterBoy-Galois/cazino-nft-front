798d841fc9cb3467949ad28e895aa0b68f83d2e4
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-06-04 14:04:39 +0200

    Switched root logic for Storybook and adapted stories

--- src/components/Spinner/Spinner.stories.tsx
+++ src/components/Spinner/Spinner.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Spinner from '.';
 
-storiesOf('Components|Spinner', module)
+storiesOf('Components/Spinner', module)
   .add('White', () => (
     <div style={{ width: '100px', height: '100px' }}>
       <Spinner color="WHITE" />
