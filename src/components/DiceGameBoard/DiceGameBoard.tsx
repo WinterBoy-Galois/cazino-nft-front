65f04a12b03c240fb771acb8a6192b6699ca3cfe
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-06-08 17:29:23 +0200

    Roughly positioned slider on scale [#172518860]

--- src/components/DiceGameBoard/DiceGameBoard.tsx
+++ src/components/DiceGameBoard/DiceGameBoard.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './DiceGameBoard.module.scss';
 import DiceResultScale from '../icons/DiceResultScale';
+import Slider from '../Slider';
 
 const DiceGameBoard: React.FC = () => {
   return (
     <div className={styles.container}>
       <DiceResultScale result="45.66" />
+      <div className={styles.slider}>
+        <Slider />
+      </div>
     </div>
   );
 };
