fcedb094b05dfedef828a397fb17448d216bef09
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-06-09 09:46:44 +0200

    Improved slider positioning [#172518860]

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -3,7 +3,16 @@ import { storiesOf } from '@storybook/react';
 import Slider from '.';
 
 storiesOf('Components/Slider', module).add('Default', () => (
-  <div style={{ display: 'flex', justifyContent: 'center', height: '500px' }}>
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '500px',
+      width: '70px',
+      margin: '0 auto',
+      paddingTop: '30px',
+    }}
+  >
     <Slider />
   </div>
 ));
