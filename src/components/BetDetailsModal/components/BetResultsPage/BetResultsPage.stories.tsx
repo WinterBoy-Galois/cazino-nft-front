285822ee0f4a18b819d276642a2eb71b25d60774
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-06-15 16:41:53 +0200

    Added custom story per game to reduce knobs overload [#172518862]

--- src/components/BetDetailsModal/components/BetResultsPage/BetResultsPage.stories.tsx
+++ src/components/BetDetailsModal/components/BetResultsPage/BetResultsPage.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import BetResultsPage from '.';
 import { GameTypes } from '../../../../models/gameTypes.model';
-import { withKnobs, select, number, boolean, array } from '@storybook/addon-knobs';
+import { withKnobs, number, boolean, array } from '@storybook/addon-knobs';
 
 storiesOf('Components/BetDetailsModal/BetResultsPage', module)
   .addDecorator(withKnobs)
@@ -11,18 +11,9 @@ storiesOf('Components/BetDetailsModal/BetResultsPage', module)
       {storyFn()}
     </div>
   ))
-  .add('Custom', () => (
+  .add('Dice', () => (
     <BetResultsPage
-      gameType={select(
-        'Game Type',
-        {
-          Dice: GameTypes.DICE,
-          Clam: GameTypes.CLAMS,
-          Mines: GameTypes.MINES,
-          Goals: GameTypes.GOALS,
-        },
-        GameTypes.DICE
-      )}
+      gameType={GameTypes.DICE}
       loading={false}
       betDetails={{
         bet: number('Bet', 0.00006874),
@@ -34,6 +25,20 @@ storiesOf('Components/BetDetailsModal/BetResultsPage', module)
           over: boolean('Over', false),
           winChance: number('Win Chance', 68.09369753713791),
           resultFloat: number('Result', 65.66),
+        },
+      }}
+    />
+  ))
+  .add('Mines', () => (
+    <BetResultsPage
+      gameType={GameTypes.MINES}
+      loading={false}
+      betDetails={{
+        bet: number('Bet', 0.00006874),
+        profit: number('Profit', 0.0000312),
+        profitCut: boolean('Profit cut', false),
+        multiplier: number('Multiplier', 1.4539),
+        gameResult: {
           mineCount: number('Mines', 24),
           minePositions: array('Mine positions', ['3', '7', '9', '15']).map(p => parseInt(p)),
           open: array('Opened fields', ['4', '5', '8', '15', '20']).map(p => parseInt(p)),
