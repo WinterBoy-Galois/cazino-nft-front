7207394eb16dad2873f32542dcff93cf09d312ce
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-06-15 13:40:16 +0200

    Improved storybook stories [#172518860]

--- src/components/BetDetailsModal/components/BetResultsPage/BetResultsPage.stories.tsx
+++ src/components/BetDetailsModal/components/BetResultsPage/BetResultsPage.stories.tsx
@@ -4,6 +4,44 @@ import BetResultsPage from '.';
 import { GameTypes } from '../../../../models/gameTypes.model';
 
 storiesOf('Components/BetDetailsModal/BetResultsPage', module)
-  .addDecorator(storyFn => <div style={{ padding: '1rem 2rem' }}>{storyFn()}</div>)
-  .add('win', () => <BetResultsPage gameType={GameTypes.DICE} loading={false} />)
-  .add('loss', () => <BetResultsPage gameType={GameTypes.DICE} loading={false} />);
+  .addDecorator(storyFn => (
+    <div style={{ padding: '1rem 2rem', backgroundColor: '#2d4560', height: '100vh' }}>
+      {storyFn()}
+    </div>
+  ))
+  .add('win', () => (
+    <BetResultsPage
+      gameType={GameTypes.DICE}
+      loading={false}
+      betDetails={{
+        bet: 0.00006874,
+        profit: 0.0000312,
+        profitCut: false,
+        multiplier: 1.4539,
+        gameResult: {
+          target: 68.09369753713791,
+          over: false,
+          winChance: 68.09369753713791,
+          resultFloat: 65.66,
+        },
+      }}
+    />
+  ))
+  .add('loss', () => (
+    <BetResultsPage
+      gameType={GameTypes.DICE}
+      loading={false}
+      betDetails={{
+        bet: 0.00006874,
+        profit: 0.0000312,
+        profitCut: false,
+        multiplier: 1.4539,
+        gameResult: {
+          target: 68.09369753713791,
+          over: true,
+          winChance: 68.09369753713791,
+          resultFloat: 85.66,
+        },
+      }}
+    />
+  ));
