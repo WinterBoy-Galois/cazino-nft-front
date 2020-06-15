8d90505196e44f8456e278c2b5f5da591e642b1f
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-06-15 17:20:52 +0200

    Added styles and images for mines results [#172518862]

--- src/components/BetDetailsModal/components/BetResultsPage/components/MinesBetResults/MinesBetResults.tsx
+++ src/components/BetDetailsModal/components/BetResultsPage/components/MinesBetResults/MinesBetResults.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import styles from './MinesBetResults.module.scss';
+import Mine from '../../../../../../assets/images/games/mines/mines-boom.svg';
+import Cash from '../../../../../../assets/images/games/mines/mines-bitcoin.svg';
 
 interface IProps {
   fieldCount: number;
@@ -8,12 +11,20 @@ interface IProps {
 
 const MinesBetResults: React.FC<IProps> = ({ fieldCount, minePositions, openedFields }) => {
   return (
-    <div>
+    <div className={styles.container}>
       {Array.from(new Array(fieldCount)).map((_, fieldIndex) => (
-        <div key={fieldIndex}>
-          <span>{fieldIndex}</span> |
-          <span>{minePositions.includes(fieldIndex) ? 'Mine' : 'Cash'}</span> |
-          <span>{openedFields.includes(fieldIndex) ? 'Open' : 'Closed'}</span>
+        <div
+          key={fieldIndex}
+          className={`${styles.field} ${
+            openedFields.includes(fieldIndex) ? styles['field--opened'] : ''
+          } ${minePositions.includes(fieldIndex) ? styles['field--loss'] : styles['field--win']}`}
+        >
+          <span className={styles.field__index}>{fieldIndex}</span>
+          {minePositions.includes(fieldIndex) ? (
+            <img src={Mine} alt="Mine" />
+          ) : (
+            <img src={Cash} alt="Cash" />
+          )}
         </div>
       ))}
     </div>
