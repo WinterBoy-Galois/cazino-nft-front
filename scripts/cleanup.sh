6f944dc9ba8033c3dc31c4fe5646f1d7b905f399
levalan <levon.alanakyan@jazva.com>
2020-06-28 13:40:49 +0400

    aa

---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "cleaning up with ${USER}"
+
+rm -rf /home/ubuntu/cc-front || true
---
+++ scripts/validate_service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# curl -v --silent localhost:8000 2>&1 | grep Connected
+# echo $?
+# exit $?
+
+echo 0
+exit 0
