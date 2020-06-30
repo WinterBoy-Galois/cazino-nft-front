093bb913385e10ec9439e7cc1cce74ad10cb76dc
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-06-30 11:01:11 +0200

    Added Storybook Background Colors

--- src/components/InputField/InputField.stories.tsx
+++ src/components/InputField/InputField.stories.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
 
 import InputField from '.';
 
 export default {
   title: 'Components/InputField',
   component: InputField,
+  parameters: {
+    backgrounds: [
+      { name: 'Primary', value: '#091b33' },
+      { name: 'Secondary', value: '#2d4560', default: true },
+    ],
+  },
 };
 
 const data = {
-  label: 'Username',
+  label: text('Label', 'Username'),
   value: 'sergioalvarez',
   onChangeValue: action('onChangeValue'),
 };
 
-export const Default = () => <InputField {...data} />;
+export const Default = () => (
+  <InputField {...data} label={text('Label', 'Username')} value={text('Value', 'sergioalvarez')} />
+);
