626793150f01fb97749f9847c63eaf14ae28c8b2
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-07-01 16:59:07 +0200

    Added styling for overlay component [#173468294]

--- src/components/AuthOverlay/AuthOverlay.tsx
+++ src/components/AuthOverlay/AuthOverlay.tsx
@@ -3,6 +3,8 @@ import { useQuery } from '@apollo/react-hooks';
 import { useStateValue } from '../../state';
 import { ME } from '../../graphql/queries';
 import { getAccessToken } from '../../common/util/storage.util';
+import styles from './AuthOverlay.module.scss';
+import Spinner from '../Spinner';
 
 const AuthOverlay: React.FC = ({ children }) => {
   const [{ auth }, dispatch] = useStateValue();
@@ -21,7 +23,16 @@ const AuthOverlay: React.FC = ({ children }) => {
     }
   }, [dispatch, data, error, auth.accessToken]);
 
-  return auth.state === 'UNKNOWN' ? <div>Just a moment</div> : <Fragment>{children}</Fragment>;
+  return auth.state === 'UNKNOWN' ? (
+    <div className={styles.container}>
+      <div className={styles.spinner}>
+        <Spinner color={'WHITE'} />
+      </div>
+      <div>Just a moment</div>
+    </div>
+  ) : (
+    <Fragment>{children}</Fragment>
+  );
 };
 
 export default AuthOverlay;
