0f29b03d5feb488a77621fa8269cc2c431cc7bbd
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-07-01 16:04:24 +0200

    Added attaching accessToken to http requests [#173468294]

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Layout from '../Layout/Layout';
 import { Router } from '@reach/router';
 import HomePage from '../../pages/home';
 import { ApolloProvider } from '@apollo/react-hooks';
 import AuthOverlay from '../AuthOverlay';
-import client from '../../graphql/client';
+import getApolloClient from '../../graphql/client';
 
 const App: React.FC = () => {
+  const client = useMemo(() => getApolloClient(), []);
+
   return (
     <ApolloProvider client={client}>
       <AuthOverlay>
--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,9 +1,5 @@
 import { ApolloClient } from 'apollo-client';
-import {
-  InMemoryCache,
-  NormalizedCacheObject,
-  IntrospectionFragmentMatcher,
-} from 'apollo-cache-inmemory';
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import { WebSocketLink } from 'apollo-link-ws';
 import { from, split } from 'apollo-link';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
@@ -13,6 +9,7 @@ import { HttpLink } from 'apollo-link-http';
 import jwtDecode from 'jwt-decode';
 import { getEpoch } from '../common/util/date.util';
 import { getAccessToken, setAccessToken } from '../common/util/storage.util';
+import { setContext } from 'apollo-link-context';
 
 // https://www.apollographql.com/docs/react/data/fragments/#fragments-on-unions-and-interfaces
 const fragmentMatcher = new IntrospectionFragmentMatcher({
@@ -25,75 +22,92 @@ const cache = new InMemoryCache({
   fragmentMatcher,
 });
 
-const wsLink = new WebSocketLink({
-  uri: `wss://staging.jinglebets.com/graphql`,
-  options: {
-    reconnect: true,
-    connectionParams: {
-      authToken: getAccessToken(),
+const getApolloClient = () => {
+  const wsLink = new WebSocketLink({
+    uri: `wss://staging.jinglebets.com/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: getAccessToken(),
+      },
     },
-  },
-});
+  });
 
-const httpLink = new HttpLink({
-  uri: 'https://staging.jinglebets.com/graphql',
-  credentials: 'include',
-});
+  const httpLink = new HttpLink({
+    uri: 'https://staging.jinglebets.com/graphql',
+    credentials: 'include',
+  });
 
-const tokenLink = new TokenRefreshLink({
-  isTokenValidOrUndefined: () => {
-    const accessToken = getAccessToken();
+  const authLink = setContext((_, { headers }) => {
+    const token = getAccessToken();
 
-    if (accessToken) {
-      const { exp } = jwtDecode(accessToken);
-
-      if (exp < getEpoch()) {
-        return false;
-      }
+    if (token) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${token}`,
+        },
+      };
     }
 
-    return true;
-  },
-  fetchAccessToken: () => {
-    return fetch('https://staging.jinglebets.com/refresh_tokens', {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${getAccessToken()}`,
-      },
-      credentials: 'include',
-    });
-  },
-  handleFetch: accessToken => setAccessToken(accessToken),
-  // handleResponse: (operation, accessTokenField) => (response: Response) => {
-  // here you can parse response, handle errors, prepare returned token to
-  // further operations
-  // returned object should be like this:
-  // {
-  //    access_token: 'token string here'
-  // }
-  // },
-  // handleError: err => {
-  //   // full control over handling token fetch Error
-  //   console.warn('Your refresh token is invalid. Try to relogin');
-  //   console.error(err);
-  // },
-});
+    return headers;
+  });
 
-const link = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      (definition.operation === 'subscription' || definition.operation === 'query')
-    );
-  },
-  wsLink,
-  httpLink
-);
+  const tokenLink = new TokenRefreshLink({
+    isTokenValidOrUndefined: () => {
+      const accessToken = getAccessToken();
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache,
-  link: from([tokenLink, link]),
-});
+      if (accessToken) {
+        const { exp } = jwtDecode(accessToken);
 
-export default client;
+        if (exp < getEpoch()) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+    fetchAccessToken: () => {
+      return fetch('https://staging.jinglebets.com/refresh_tokens', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${getAccessToken()}`,
+        },
+        credentials: 'include',
+      });
+    },
+    handleFetch: accessToken => setAccessToken(accessToken),
+    // handleResponse: (operation, accessTokenField) => (response: Response) => {
+    // here you can parse response, handle errors, prepare returned token to
+    // further operations
+    // returned object should be like this:
+    // {
+    //    access_token: 'token string here'
+    // }
+    // },
+    // handleError: err => {
+    //   // full control over handling token fetch Error
+    //   console.warn('Your refresh token is invalid. Try to relogin');
+    //   console.error(err);
+    // },
+  });
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        (definition.operation === 'subscription' || definition.operation === 'query')
+      );
+    },
+    wsLink,
+    authLink.concat(httpLink)
+  );
+
+  return new ApolloClient({
+    cache,
+    link: from([tokenLink, link]),
+  });
+};
+
+export default getApolloClient;
