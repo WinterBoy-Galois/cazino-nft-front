e7c86b6a71ea5acff0e9ac734c37b62eac2c236c
Tim Hoffmann <tim.hoffmann@egonoid.com>
2020-07-06 15:19:05 +0200

    Added input fields to signUp form [#173468299]

--- src/components/SignUpModal/SignUpModal.tsx
+++ src/components/SignUpModal/SignUpModal.tsx
@@ -6,6 +6,12 @@ import { GraphQLError } from 'graphql';
 import { useMutation } from '@apollo/react-hooks';
 import { useStateValue } from '../../state';
 import { SIGN_UP } from '../../graphql/mutations';
+import TextInput from '../TextInput';
+import PasswordInput from '../PasswordInput';
+import SecondaryButton from '../SecondaryButton';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import signUpIllustration from '../../assets/images/auth/sign-up.svg';
 
 interface IProps {
   show: boolean;
@@ -16,10 +22,85 @@ interface IProps {
 }
 
 const SignUpModal: React.FC<IProps> = ({ show, onClose, onSignUp = () => null }: IProps) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      username: '',
+    },
+    validateOnMount: false,
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required('Please enter your Email.').email('Please enter a valid Email.'),
+      username: Yup.string().required('Please enter your Username.'),
+      password: Yup.string()
+        .required('Please enter your password.')
+        .min(8, ({ min }) => `Password is too short - length must be at least ${min} characters.`)
+        .max(20, ({ max }) => `Password is too long - maximum length is ${max} characters.`)
+        .matches(/[A-Z]/, 'Must contain at least one uppercase character.')
+        .matches(/[a-z]/, 'Must contain at least one lowercase character.')
+        .matches(/[0-9]/, 'Must contain at least one number.')
+        .matches(
+          /[\^$*.[\]{}()?\-\\"!@#%&/\\,><':;|_~`]/,
+          'Your password must contain at least one of the following special characters: ^ $ * . [ ] { } ( ) ? - " ! @ # % & / \\ , > < \' : ; | _ ~ `'
+        ),
+    }),
+    onSubmit: values => {
+      onSignUp(values.email, values.password, values.username, '');
+    },
+  });
+
   return (
     <Modal show={show} onClose={onClose} title="Sign Up">
       <div className="row">
-        <div className="col-12 col-md-7">Hi</div>
+        <div className="col-12 col-md-7">
+          <form onSubmit={formik.handleSubmit}>
+            <TextInput
+              label="username"
+              name="username"
+              onChangeValue={v => formik.setFieldValue('username', v)}
+              onBlur={formik.handleBlur}
+              value={formik.values.username}
+              {...(formik.touched.username ? { validationMessage: formik.errors.username } : {})}
+            />
+
+            <PasswordInput
+              label="password"
+              name="password"
+              onChangeValue={v => formik.setFieldValue('password', v)}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              {...(formik.touched.password ? { validationMessage: formik.errors.password } : {})}
+            />
+
+            <PasswordInput
+              label="confirm password"
+              name="confirmPassword"
+              onChangeValue={v => formik.setFieldValue('confirmPassword', v)}
+              onBlur={formik.handleBlur}
+              value={formik.values.confirmPassword}
+              {...(formik.touched.confirmPassword
+                ? { validationMessage: formik.errors.confirmPassword }
+                : {})}
+            />
+
+            <TextInput
+              label="email"
+              name="email"
+              onChangeValue={v => formik.setFieldValue('email', v)}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              {...(formik.touched.email ? { validationMessage: formik.errors.email } : {})}
+            />
+
+            <SecondaryButton type="submit" {...(formik.isValid ? {} : { disabled: true })}>
+              Sign Up
+            </SecondaryButton>
+          </form>
+        </div>
+        <div className={`col-12 col-md-5 ${styles.illustration}`}>
+          <img src={signUpIllustration} alt="Sign Up Character" />
+        </div>
       </div>
     </Modal>
   );
@@ -46,7 +127,7 @@ const SignUpModalWithData: React.FC<IWithDataProps> = ({ show, onClose }: IWithD
       return;
     }
 
-    dispatch({ type: 'AUTH_SIGN_UP', payload: { ...data.signIn } });
+    dispatch({ type: 'AUTH_SIGN_UP', payload: { ...data.signUp } });
     dispatch({ type: 'MODAL_HIDE' });
   };
 
