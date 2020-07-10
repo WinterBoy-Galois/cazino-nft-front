4ed1d6dfa7c42b768ffbd4d86feadb7568f46e80
Andreas Hoffmann <Andreas.Hoffmann@egonoid.com>
2020-07-10 05:38:38 +0200

    Reduced Passwort validation constraints for sign-in

--- src/components/SignInModal/SignInModal.tsx
+++ src/components/SignInModal/SignInModal.tsx
@@ -40,17 +40,7 @@ const SignInModal: React.FC<IProps> = ({
     isInitialValid: false,
     validationSchema: Yup.object().shape({
       email: Yup.string().required('Please enter your Email.').email('Please enter a valid Email.'),
-      password: Yup.string()
-        .required('Please enter your password.')
-        .min(8, ({ min }) => `Password is too short - length must be at least ${min} characters.`)
-        .max(20, ({ max }) => `Password is too long - maximum length is ${max} characters.`)
-        .matches(/[A-Z]/, 'Must contain at least one uppercase character.')
-        .matches(/[a-z]/, 'Must contain at least one lowercase character.')
-        .matches(/[0-9]/, 'Must contain at least one number.')
-        .matches(
-          /[\^$*.[\]{}()?\-\\"!@#%&/\\,><':;|_~`]/,
-          'Your password must contain at least one of the following special characters: ^ $ * . [ ] { } ( ) ? - " ! @ # % & / \\ , > < \' : ; | _ ~ `'
-        ),
+      password: Yup.string().required('Please enter your password.'),
     }),
     onSubmit: values => {
       onSignIn(values.email, values.password);
