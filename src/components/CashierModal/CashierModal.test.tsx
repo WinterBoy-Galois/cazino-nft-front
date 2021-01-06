ff08aefb638b973ff70082079d95c1f3e71bec1c
Mac <mac@Macs-Air.mshome.net>
2021-01-06 02:17:09 -0800

    add cashiermodaltest

--- src/components/CashierModal/CashierModal.test.tsx
+++ src/components/CashierModal/CashierModal.test.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CashierModal from '.';
+
 describe('CashierModal', () => {
-  it('should be truthy', () => {
+  xit('should match snapshot', () => {
     // Arrange
 
     // Act
+    const container = render(
+      <CashierModal
+        show
+        loading={false}
+        cashier={{
+          networkFee: 0.00075295,
+          depositConfirmations: 2,
+          minWithdraw: 2e-7,
+        }}
+        depositAddress={'3CaQi76neM8qoDh18pLq5W8Hru1sNVRoDY'}
+      />
+    );
+
     // Assert
-    expect(true).toBeTruthy();
+    expect(container).toMatchSnapshot();
   });
 });
