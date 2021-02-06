67fd1991b2da83348161d2707f4a87930381b789
square1grp <developer01@square1grp.com>
2021-02-06 09:43:06 -0800

    menu component

--- src/components/Layout/components/BottomBar/components/Menu/Menu.test.tsx
+++ src/components/Layout/components/BottomBar/components/Menu/Menu.test.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
 import Menu from './Menu';
+import { LocationProvider } from "@reach/router";
+import { FAUCET_INFO } from '../../../../../../graphql/queries';
 
+const mocks = [
+  {
+    request: {
+      query: FAUCET_INFO,
+    },
+    result: {
+      data: {
+        faucetInfo: { "amount": 3300331, "every": 30, "canClaim": true, "__typename": "FaucetInfo" },
+      },
+    },
+  },
+];
 describe('Menu', () => {
   it('should match snapshot', () => {
     // Arrange
 
     // Act
-    const container = render(<Menu />);
+    const container = render(
+      <LocationProvider>
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Menu />
+        </MockedProvider>
+      </LocationProvider>
+    );
 
     // Assert
     expect(container).toMatchSnapshot();
