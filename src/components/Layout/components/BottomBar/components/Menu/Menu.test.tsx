cef9e7b6b714ff818e408bdba5f552969f4d991f
square1grp <developer01@square1grp.com>
2021-02-08 00:06:57 -0800

    Make sure withdraw toasts are coming

--- src/components/Layout/components/BottomBar/components/Menu/Menu.test.tsx
+++ src/components/Layout/components/BottomBar/components/Menu/Menu.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import Menu from './Menu';
-import { LocationProvider } from "@reach/router";
+import { LocationProvider } from '@reach/router';
 import { FAUCET_INFO } from '../../../../../../graphql/queries';
 
 const mocks = [
@@ -12,7 +12,7 @@ const mocks = [
     },
     result: {
       data: {
-        faucetInfo: { "amount": 3300331, "every": 30, "canClaim": true, "__typename": "FaucetInfo" },
+        faucetInfo: { amount: 3300331, every: 30, canClaim: true, __typename: 'FaucetInfo' },
       },
     },
   },
