7a84b07174d0e26bb4b437d25cc39fdd2ea021d8
square1grp <developer01@square1grp.com>
2021-02-07 23:57:36 -0800

    Make sure withdraw toasts are coming

---
+++ src/components/WithdrawalsTable/WithdrawalsTable.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useLocation, useNavigate } from '@reach/router';
+import { datetimeFromEpoch } from '../../common/util/date.util';
+import { formatBitcoin } from '../../common/util/format.util';
+import TransactionsWithdraw from '../../models/transactionsWithdraw.model';
+import BitcoinValue from '../BitcoinValue';
+import TransactionsTable from '../TransactionsTable';
+import { TableColumn } from '../TransactionsTable/lib/tableColumn';
+import BitcoinProfit from '../BitcoinProfit';
+import GameIcon from '../GameIcon';
+import styles from './WithdrawalsTable.module.scss';
+import { useStateValue } from '../../state';
+interface IProps {
+  data: TransactionsWithdraw[];
+  paginationTotalRows?: number;
+  onChangePage?: (page: number) => void;
+  onChangeRowsPerPage?: (row: number) => void;
+  loading?: boolean;
+}
+
+const WithdrawalsTable: React.FC<IProps> = props => {
+  const columns: TableColumn<TransactionsWithdraw>[] = [];
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [{ auth }] = useStateValue();
+  const handleRowClicked = (row: TransactionsWithdraw) => {
+    navigate(`${pathname}?dialog=withdraw-details`, {
+      state: {},
+    });
+  };
+
+  return (
+    <TransactionsTable<TransactionsWithdraw>
+      {...props}
+      columns={columns}
+      progressPending={props.loading}
+      onRowClicked={handleRowClicked}
+    />
+  );
+};
+
+export default WithdrawalsTable;
---
+++ src/pages/TransactionsPage/components/Withdrawals/Withdrawals.tsx
@@ -0,0 +1,60 @@
+import { useQuery } from '@apollo/client';
+import { RouteComponentProps } from '@reach/router';
+import React, { useState } from 'react';
+import WithdrawalsTable from '../../../../components/WithdrawalsTable';
+import { TRANSACTION_WITHDRAWALS } from '../../../../graphql/queries';
+import useRefetchOnPageEnter from '../../../../hooks/useRefetchOnPageEnter.hook';
+import TransactionsWithdraw from '../../../../models/transactionsWithdraw.model';
+import styles from './Withdrawals.module.scss';
+
+interface IProps {
+  withdrawls: TransactionsWithdraw[];
+  loading?: boolean;
+  onChangePage?: (page: number) => void;
+  onChangeRowsPerPage?: (row: number) => void;
+  paginationTotalRows?: number;
+}
+
+const Withdrawals: React.FC<IProps> = ({
+  withdrawls,
+  onChangePage,
+  onChangeRowsPerPage,
+  loading,
+  paginationTotalRows,
+}) => {
+  return (
+    <div className={styles.container}>
+      <WithdrawalsTable
+        data={withdrawls}
+        onChangePage={onChangePage}
+        onChangeRowsPerPage={onChangeRowsPerPage}
+        loading={loading}
+        paginationTotalRows={paginationTotalRows}
+      />
+    </div>
+  );
+};
+
+export default Withdrawals;
+
+export const WithdrawalsWithData: React.FC<RouteComponentProps> = () => {
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const { data, loading, refetch } = useQuery(TRANSACTION_WITHDRAWALS, {
+    variables: { page: page - 1, limit },
+  });
+
+  useRefetchOnPageEnter(refetch, loading);
+
+  console.log(data);
+
+  return (
+    <Withdrawals
+      withdrawls={data?.transactionsWithdraw?.items}
+      onChangePage={p => setPage(p)}
+      onChangeRowsPerPage={l => setLimit(l)}
+      loading={loading}
+      paginationTotalRows={data?.transactionsBet?.total}
+    />
+  );
+};
