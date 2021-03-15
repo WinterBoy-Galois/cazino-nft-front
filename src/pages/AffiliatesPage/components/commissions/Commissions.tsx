d8a49d60f8628519c03174acb8b6f167d90c40ef
serhiy1017 <s.bezpalyi@outlook.com>
2021-03-15 14:42:26 +0800

    Completing the whole sub-milestones

--- src/pages/AffiliatesPage/components/commissions/Commissions.tsx
+++ src/pages/AffiliatesPage/components/commissions/Commissions.tsx
@@ -5,13 +5,19 @@ import { formatBitcoin } from '../../../../common/util/format.util';
 import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
 import { useStateValue } from '../../../../state';
+import SpinnerButton from '../../../../components/SpinnerButton';
 
 interface IProps {
   data?: any;
   onTransferBalance?: () => void;
+  loadingSetup?: boolean;
 }
 
-const Commissions: React.FC<IProps> = ({ data: data, onTransferBalance = () => null }) => {
+const Commissions: React.FC<IProps> = ({
+  data: data,
+  onTransferBalance = () => null,
+  loadingSetup,
+}) => {
   const { t } = useTranslation(['affiliates']);
   const [{ sidebar }] = useStateValue();
   const onTransfer = () => {
@@ -19,7 +25,6 @@ const Commissions: React.FC<IProps> = ({ data: data, onTransferBalance = () => n
       onTransferBalance();
     }
   };
-  console.log(data);
   return (
     <div>
       <div className={styles.sub_title}>{t('commissions')}</div>
@@ -27,20 +32,23 @@ const Commissions: React.FC<IProps> = ({ data: data, onTransferBalance = () => n
         <div className={clsx(sidebar?.isOpen ? styles.btn_width : styles.btn_width_close)}>
           <div className={styles.btn_amount}>
             <div className={styles.flex_amount}>
-              <div>{t('amount')}</div>
+              <div className={styles.txt_color}>{t('amount')}</div>
               <div>
                 <BitcoinValue value={formatBitcoin(data?.refCommissions)} />
               </div>
             </div>
           </div>
-          <div
+          <SpinnerButton
             className={clsx(
               data?.refCommissions === 0 ? styles.btn_transfer_disable : styles.btn_transfer
             )}
+            loading={loadingSetup}
+            loadingText={t('waiting')}
             onClick={onTransfer}
+            disabled={loadingSetup}
           >
-            <div className={styles.flex_btn}>{t('transfer')}</div>
-          </div>
+            {t('transfer')}
+          </SpinnerButton>
         </div>
         <div className={clsx(sidebar?.isOpen ? styles.bg_width : styles.bg_width_close)} />
       </div>
