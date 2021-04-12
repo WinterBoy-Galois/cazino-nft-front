265cf69a7c2e0910dfa513000cac29c8a3a56563
serhiy1017 <s.bezpalyi@outlook.com>
2021-04-12 12:40:09 -0400

    Implementing the chatbot feature

---
+++ src/customerSupport/CustomerSupport.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+// import tawkTo from './customerSupport';
+
+const __customerSupport: React.FC = () => {
+  useEffect(() => {
+    // tawkTo();
+  }, []);
+  return null;
+};
+
+export default __customerSupport;
